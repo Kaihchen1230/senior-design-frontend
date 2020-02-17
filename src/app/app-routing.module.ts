f3eb8f04a777a1b2e18d3982e54cdc55a190ea29
kaihchen1230 <kaihang.c@gmail.com>
2020-02-16 23:11:28 -0500

    add home component and search result component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { HomeComponent } from "./home/home.component";
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { SearchResultComponent } from './search-result/search-result.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'search-result', component: SearchResultComponent }
+
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchResultComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
