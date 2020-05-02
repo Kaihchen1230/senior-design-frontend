796aff2daf32f78ab955aaaaa904d5c413b625a0
Jie Lan <lanjie3458@gmail.com>
2020-05-02 15:56:12 -0400

    clean up code in search-result.ts

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -10,27 +10,14 @@ import { Repo } from '../shared/repo.model';
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.css']
 })
-export class SearchResultComponent implements OnInit {
 
+export class SearchResultComponent implements OnInit {
   searchTerm = '';
   searchIcon = faSearch;
   starIcon = faStar;
-  incresing = faCaretUp;
-  decresing = faCaretDown;
-  languagess = [
-    'C++',
-    'Java',
-    'Python',
-    'JavaScript',
-    'PHP',
-    'C#',
-    'C',
-    'R',
-  ];
   languages = {};
   isFetching = false;
   searchResults: Repo[] = [];
-  noRepo = false;
   error = null;
 
   constructor(private searchService: SearchService,
@@ -39,38 +26,30 @@ export class SearchResultComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    // const searchTerm = this.searchService.searchTerm;
-
-
     this.searchTerm = localStorage.getItem('searchTerm');
-    console.log('is fetching');
     this.route.params
       .subscribe(
         (params: Params) => {
           this.isFetching = true;
           this.error = null;
           const searchTerm = params['search-term'];
-          this.searchTerm = searchTerm;
-          console.log('this is search term from params: ', searchTerm);
 
           this.requestRepoService.fetchRepos(searchTerm).subscribe(responses => {
             this.searchResults = [...responses];
-            console.log('this is searchResults: ', this.searchResults);
             localStorage.setItem('searchResults', JSON.stringify(this.searchResults));
 
             if (responses.length === 0) {
               this.error = 'No Repos related to the keyword: ';
             }
 
-            // const languages = {};
             this.searchResults.forEach((searchResult: any) => {
 
               let language = searchResult.language;
-              // console.log('this is language: ', language);
+
               if (!language) {
                 language = 'Unknow';
               }
-              // const tmpLanguage = language.toLowerCase();
+
               language = language.toLowerCase();
 
               if (language === 'javascript') {
@@ -80,18 +59,11 @@ export class SearchResultComponent implements OnInit {
               }
 
               if (this.languages.hasOwnProperty(language)) {
-                // console.log('tmpLanguage in if : ', tmpLanguage);
                 this.languages[language] += 1;
               } else {
-                // console.log('tmpLanguage in else: ', tmpLanguage);
                 this.languages[language] = 1;
               }
             });
-
-            // this.languages = {... languages};
-            // console.log(' Object.keys(languages): ',  Object.keys(languages));
-
-            // console.log('this.languages: ', this.languages);
 
             this.isFetching = false;
           }, error => {
@@ -101,28 +73,14 @@ export class SearchResultComponent implements OnInit {
 
           });
           console.log(this.searchResults);
-          if (this.searchResults.length === 0) {
-            this.noRepo = true;
-          }
       });
   }
 
-  randomNumIsEven(){
-    const num: number =  Math.floor(Math.random() * 100);
-    // console.log('this is num: ', num);
-    return num % 2 === 0;
-  }
-
   goToDetailPage(repoName: string, platform: string) {
-
     this.searchService.repoName = repoName;
-    // localStorage.setItem('repoName', repoName);
-    // localStorage.setItem('platform', platform);
-    // this.router.navigate(['/detail-page']);
   }
 
   onHandleError() {
-    this.noRepo = false;
     this.router.navigate(['/']);
   }
 
