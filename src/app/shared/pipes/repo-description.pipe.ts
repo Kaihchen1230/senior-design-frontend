710df027dbf9eb433b51e7075529d720d85acae8
Jie Lan <lanjie3458@gmail.com>
2020-05-28 20:31:11 -0400

    fix empty description error

--- src/app/shared/pipes/repo-description.pipe.ts
+++ src/app/shared/pipes/repo-description.pipe.ts
@@ -6,6 +6,9 @@ If length > 200, add "..." to the end
 @Pipe({name: 'RepoDesciptionPipe'})
 export class RepoDesciptionPipe implements PipeTransform {
     transform(description: string, maxLength: number): string {
+        if (!description) {
+            return null;
+        }
         let newDes: string = description.substring(0, maxLength);
         if (description.length > maxLength) {
             newDes += '...';
